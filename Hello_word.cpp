f21610b27f1d2295fbee66958089033a77414fe2
Akash Anil Sakhare <114383319+Akashsakhare57@users.noreply.github.com>
2023-01-02 21:44:41 +0530

    Create Hello_word.cpp

---
+++ Hello_word.cpp
@@ -0,0 +1,19 @@
+/*
+* Multiple line
+* comment
+*/
+#include<iostream>         
+//This statements tells the compiler to include iostream file. This file contains pre defined input/output functions that we can use in our program
+
+//Single line comment
+using namespace std;
+//A namespace is like a region, where we have functions, variables etc and their scope is limited to that particular region. Here std is a namespace name, this tells the compiler to look into that particular region for all the variables, functions, etc.
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout<<"Hello World!";
+
+   return 0;
+}
